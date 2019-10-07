8adc546c5a06b8a0054b413700815c5357de71a1
olegs7771 <olegs7771@gmail.com>
2019-10-07 13:57:22 +0300

    ok

--- app/screens/DrawMenu/ThirdItem.js
+++ app/screens/DrawMenu/ThirdItem.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 export class ThirdItem extends Component {
   render() {
     return (
       <View>
         <Text> Third Item </Text>
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />
       </View>
     );
   }
